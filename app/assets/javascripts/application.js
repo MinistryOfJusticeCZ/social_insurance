d2c62adcad1e2d383685d3336ddfcebf891757d5
Jarko Hlavso <jhlavsa@labjarko01.lab.justice.cz>
2018-11-27 14:03:12 +0100

    next step

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,5 @@
 //= require jquery-ui/widget
 //= require turbolinks
 //= require azahara_schema/application
-//= require egov_utils/application
-//= require_tree .
+//= require popper
+//= require bootstrap-sprockets
